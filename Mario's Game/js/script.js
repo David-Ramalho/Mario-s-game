1706db30739c3e8f3cce2ded5bf49e086d8ea694
David-Ramalho <tavaresramalho@hotmail.com>
2022-06-30 19:42:06 -0300

    Mario's game improvements
    Some work on the JS part of the game.

--- Mario's Game/js/script.js
+++ Mario's Game/js/script.js
@@ -3,7 +3,7 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
 
-//variavel constante jump e arrow function. The left part denotes the input of a function and the right part the output of that function.
+//var constante jump e arrow function. The left part denotes the input of a function and the right part the output of that function.
 const jump = () => {
  mario.classList.add('jump');
 
@@ -17,14 +17,31 @@ const jump = () => {
 
 //var receb fun e um tempo de execução
 const loop = setInterval(() => {
+    console.log('loop')
 
     const pipePosition = pipe.offsetLeft;
     console.log(pipePosition);
 
-    if (pipePosition <= 120) {
+    //const para pegar o buttom. N pode usar offset. getcomp.style  returns an object containing the values of all CSS properties of an element.
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    console.log(marioPosition);
+
+    // .replace('px', '');= substitui a palavra PX para nada. 
+    // o + na frente converte para number.
+
+    if (pipePosition <= 120 && pipePosition>0 && marioPosition<80) {
         pipe.style.animation ='none';
-        pipe.style.left = `${pipePosition}px`;                       
-    }
+        pipe.style.left = `${pipePosition}px`;  
+        
+        mario.style.animation ='none';
+        mario.style.bottom = `${marioPosition}px`; 
+
+        mario.src = './images/game-over.png';
+        mario.style.width ='80px';
+        mario.style.marginLeft ='50px'
+    
+        clearInterval(loop);
+}
     
 }, 10);
 
